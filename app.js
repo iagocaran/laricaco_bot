fd1242ccbd54b0b9ee2cb80ed4dd9f7d04923f94
Iago Caran Aquino <iago.caran@conpec.com.br>
2018-09-05 08:47:03 -0300

    Made it send a message on request.

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const axios = require('axios');
 
-const database = require('database.js');
+// const database = require('database.js');
 
 const indexRouter = require('./routes/index');
 
@@ -15,7 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('database', database);
+app.set('express', express);
+app.set('axios', axios);
+// app.set('database', database);
+app.set('telegramToken', process.env.TELEGRAM_TOKEN);
 
 app.use('/', indexRouter(app));
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const router = express.Router();
-const database = app.get('database');
-
 module.exports = (app) => {
+	const router = app.get('express').Router();
+	const axios = app.get('axios');
+	const api = 'https://api.telegram.org/' + app.get('telegramToken');
+// const database = app.get('database');
+
+
 	/* GET home page. */
 	router.get('/', function(req, res, next) {
 		res.send('It\'s me Mario');
+	});
+
+	router.post('/', (req, res, next) => {
+		try {
+			let msg = req.body.message;
+			axios.post(api + '/send', {
+				chat_id: msg.chat.id,
+				text: 'Hoje tem IFCH?',
+				parse_mode: 'HTML',
+				disable_notification: true,
+				reply_to_message_id: msg.message_id
+			});
+		} catch (err) {
+			console.log(err);
+		}
 	});
 
 	return router;
