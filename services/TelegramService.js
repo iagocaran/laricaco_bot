907dca205d5d314eba024887b1851f4df2ed5309
Iago Caran Aquino <iago.caran@conpec.com.br>
2018-09-05 11:37:26 -0300

    Changed bitwise or's that should be normal or's.

--- services/TelegramService.js
+++ services/TelegramService.js
@@ -3,11 +3,12 @@ module.exports = (app) => {
 	const api = 'https://api.telegram.org/bot' + app.get('telegramToken');
 
 	function sendMessage (chat_id, text, reply_to_message_id, disable_notification, parse_mode) {
+		console.log()
 		return axios.post(api + '/sendMessage', {
 			chat_id,
-			text: text | '',
-			parse_mode: parse_mode | 'HTML',
-			disable_notification: disable_notification | false,
+			text: text || '',
+			parse_mode: parse_mode || 'HTML',
+			disable_notification: disable_notification || false,
 			reply_to_message_id
 		})
 	}
