8851a3a8ec27d40d3125bd379171081f00230311
Iago Caran Aquino <iago.caran@conpec.com.br>
2018-09-05 15:19:02 -0300

    Agora esse switch tem que estar certo.

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
 		if (message.text.startsWith('/')) {
 			let match = message.text.match(/\/.+?[@\s]|\/.+\b/)[0];
 			console.log(match.substr(1, match.length).replace(/@laricaco_bot/, ''));
-			switch (match.substr(1, match.length).replace(/@laricaco_bot/, '')) {
+			switch (match.substr(1, match.length).replace(/@/, '')) {
 				case "precos":
 					msg = "precos";
 					break;
